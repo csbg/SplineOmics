6c7be93372b8e1c338eb97489d4f3791e653b5f1
Thomas-Rauter <thomas.rauter@plus.ac.at>
2024-07-15 18:38:49 +0200

    x

--- dev/function_testing_ground.R
+++ dev/function_testing_ground.R
@@ -20,10 +20,9 @@ library(dplyr)
 #                               "Time_course_PPTX_old_metadata.xlsx"))
 
 
-data_excel <- read_excel(here::here(
-  "inst" ,"extdata", "proteomics_data.xlsx"))
+data_excel <- read_excel(here::here("inst" ,"extdata", "data.xlsx"))
 
-meta <- read_excel(here::here("inst" ,"extdata", "proteomics_meta.xlsx"))
+meta <- read_excel(here::here("inst" ,"extdata", "meta.xlsx"))
 
 annotation <- data_excel %>%
   select(39:ncol(data_excel)) %>%  
@@ -54,19 +53,11 @@ data <- extract_data(data_excel,
 
 # Remove outliers --------------------------------------------------------------
 
-col_names <- colnames(data)
-
-# Identify the columns to remove
-cols_to_remove <- c("E12_TP05_Exponential", "E10_TP10_Stationary")
-
-# Find the indices of the columns to remove
-cols_to_remove_indices <- which(col_names %in% cols_to_remove)
-
-# Remove the columns by subsetting
-data <- data[, -cols_to_remove_indices]
-
-meta <- meta %>%
-  dplyr::filter(!Sample.ID %in% c("E12_TP05_Exponential", "E10_TP10_Stationary"))
+# data <- data %>%
+#   select(-E12_TP05_Exponential, -E10_TP10_Stationary)
+# 
+# meta <- meta %>%
+#   filter(!Sample.ID %in% c("E12_TP05_Exponential", "E10_TP10_Stationary"))
 
 # Explore data -----------------------------------------------------------------
 
@@ -92,12 +83,11 @@ splineomics <- create_splineomics(
 report_dir <- here::here("results", "explore_data")
 
 # debug(explore_data)
-plots <- explore_data(
-  splineomics,
-  report_dir = report_dir,
-  report = TRUE
-  )
-
+# plots <- explore_data(
+#   splineomics,
+#   report_dir = report_dir,
+#   report = TRUE
+#   )
 
 
 # Prep input to hyperparams screen function ------------------------------------
@@ -143,8 +133,8 @@ spline_test_configs <- data.frame(spline_type = c("n", "n", "n", "n"),
 splineomics <- update_splineomics(
   splineomics = splineomics,
   design = "~ 1 + Phase*X + Reactor",
-  data = data1,
-  meta = meta1,
+  data = data2,
+  meta = meta2,
   spline_params = list(spline_type = c("n"),   # Chosen spline parameters
                        dof = c(2L))
 )
@@ -157,7 +147,7 @@ splineomics <- run_limma_splines(
 
 report_dir <- here::here("results", "limma_reports")
 
-# plots <- create_limma_report(
+# create_limma_report(
 #   splineomics,
 #   report_dir = report_dir
 #   )
@@ -166,14 +156,14 @@ report_dir <- here::here("results", "limma_reports")
 
 ## Cluster hits ----------------------------------------------------------------
 adj_pthresholds <- c(0.05, 0.05)   
-clusters <- list(6L, 3L)   
+clusters <- list(2L, 2L)   
 report_dir <- here::here("results", "clustering_reports")
 
 plot_info = list(
   y_axis_label = "log2 intensity",
   time_unit = "min",
-  treatment_labels = c("Feeding"),
-  treatment_timepoints = c(0)
+  treatment_labels = c("Feeding", "Test"),
+  treatment_timepoints = c(10, 200)
 )
 
 # debug(cluster_hits)
@@ -191,55 +181,49 @@ clustering_results <- cluster_hits(
 
 # Perform gsea -----------------------------------------------------------------
 
-gene_set_lib <- c(
-  "WikiPathways_2019_Human",
-  "NCI-Nature_2016",
-  "TRRUST_Transcription_Factors_2019",
-  "MSigDB_Hallmark_2020",
-  "GO_Cellular_Component_2018",
-  "CORUM",
-  "KEGG_2019_Human",
-  "TRANSFAC_and_JASPAR_PWMs",
-  "ENCODE_and_ChEA_Consensus_TFs_from_ChIP-X",
-  "GO_Biological_Process_2018",
-  "GO_Molecular_Function_2018",
-  "Human_Gene_Atlas"
-  )
+gene_set_lib <- c("WikiPathways_2019_Human",
+                  "NCI-Nature_2016",
+                  "TRRUST_Transcription_Factors_2019",
+                  "MSigDB_Hallmark_2020",
+                  "GO_Cellular_Component_2018",
+                  "CORUM",
+                  "KEGG_2019_Human",
+                  "TRANSFAC_and_JASPAR_PWMs",
+                  "ENCODE_and_ChEA_Consensus_TFs_from_ChIP-X",
+                  "GO_Biological_Process_2018",
+                  "GO_Molecular_Function_2018",
+                  "Human_Gene_Atlas")
 
 # download_enrichr_databases(gene_set_lib)
 
 # Get gene vector
 data <- as.data.frame(data_excel)
-gene_column_name <- "Genes"
+gene_column_name <- "Gene Names"
 genes <- data[[gene_column_name]][4:nrow(data)]
 genes <- sub(" .*", "", genes)
 genes <- sub(";.*", "", genes)
 genes <- sub("_.*", "", genes)
-genes <- sub("-.*", "", genes)
 
 
 downloaded_dbs_filepath <- 
-  here::here("dev", "data", "all_databases_08_04_2024-12_41_50.tsv")
+  here::here("data", "all_databases_08_04_2024-12_41_50.tsv")
 
 databases <- readr::read_tsv(downloaded_dbs_filepath, col_types = readr::cols())
 
-clusterProfiler_params <- list(
-  adj_p_value = 0.05,
-  pAdjustMethod = "BH",
-  minGSSize = 10,
-  maxGSSize = 500,
-  qvalueCutoff = 0.2
-  )
+clusterProfiler_params <- list(adj_p_value = 0.05,
+                               pAdjustMethod = "BH",
+                               minGSSize = 10,
+                               maxGSSize = 500,
+                               qvalueCutoff = 0.2)
 
 report_dir <- here::here("results", "gsea_reports")
 
-# debug(create_gsea_report)
-result <- create_gsea_report(
-  levels_clustered_hits = clustering_results$clustered_hits_levels,
-  genes = genes,
-  databases = databases,
-  params = clusterProfiler_params,
-  report_info = report_info,
-  report_dir = report_dir
-  )
+# debug(run_gsea)
+result <- create_gsea_report(levels_clustered_hits = 
+                                clustering_results$clustered_hits_levels,
+                              genes = genes,
+                              databases = databases,
+                              params = clusterProfiler_params,
+                              report_info = report_info,
+                              report_dir = report_dir)
 
